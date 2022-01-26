82dddfb0c9aeecfaf343a98806999a826a598534
Jake P <jpetersen6202@gmail.com>
2022-01-25 20:45:39 -0600

    added delete functionality on delete button

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -5,9 +5,12 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts'
 
 export default function Post({post, setCurrentId}) {
     const classes = useStyles()
+    const dispatch = useDispatch()
     
     return (
         <Card className={classes.card}>
@@ -18,7 +21,7 @@ export default function Post({post, setCurrentId}) {
             </div>
             <div className={classes.overlay2}>
                 <Button style={{color: 'white'}} size='small' onClick={() => {setCurrentId(post._id)}}>
-                    <MoreHorizIcon fontSize='default' />
+                    <MoreHorizIcon fontSize='medium' />
                 </Button>
             </div>
             <div className={classes.details}>
@@ -28,7 +31,7 @@ export default function Post({post, setCurrentId}) {
             </div>
             <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant='h5' gutterBottom>{post.message}</Typography>
+                <Typography variant='body1' gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size='small' color='primary' onClick={() => {}}>
@@ -36,7 +39,7 @@ export default function Post({post, setCurrentId}) {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small' />
                     Delete
                 </Button>
